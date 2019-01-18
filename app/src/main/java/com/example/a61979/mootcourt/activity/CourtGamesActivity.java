bdbf927ca4bbdfe65b44b986e2c79156b1a20f37
VirgilG72 <619790446@qq.com>
2019-01-18 15:57:44 +0800

    version4.0模拟法庭界面初步完成

---
+++ app/src/main/java/com/example/a61979/mootcourt/activity/CourtGamesActivity.java
@@ -0,0 +1,15 @@
+package com.example.a61979.mootcourt.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.a61979.mootcourt.R;
+
+public class CourtGamesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.court_games);
+    }
+}
--- app/src/main/java/com/example/a61979/mootcourt/pager/Horizon.java
+++ app/src/main/java/com/example/a61979/mootcourt/pager/Horizon.java
@@ -1,12 +1,10 @@
 package com.example.a61979.mootcourt.pager;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.view.Gravity;
-import android.widget.TextView;
+import android.content.Intent;
 
+import com.example.a61979.mootcourt.activity.CourtGamesActivity;
 import com.example.a61979.mootcourt.base.BasePager;
-import com.example.a61979.mootcourt.utils.LogUtil;
 
 /**
  * @author Admin
@@ -16,25 +14,34 @@ import com.example.a61979.mootcourt.utils.LogUtil;
  * @updateDes ${TODO}
  */
 public class Horizon extends BasePager {
+
+
     public Horizon(Context context) {
         super(context);
     }
 
+
     @Override
     public void initData() {
         super.initData();
-        LogUtil.e("视图页面数据被初始化了。");
-        //1、设置标题
-        tv_title.setText("视界");
-        //2、联网请求得到数据，创建视图
-        TextView textView = new TextView(context);
+        Intent intent = new Intent(context,CourtGamesActivity.class);
+        context.startActivity(intent);
 
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        textView.setTextSize(25);
-        fl_content.addView(textView);
-        //3、绑定数据
-        textView.setText("视界内容");
+
+//        LogUtil.e("视图页面数据被初始化了。");
+//        //1、设置标题
+//        tv_title.setText("视界");
+//        //2、联网请求得到数据，创建视图
+//        TextView textView = new TextView(context);
+//
+//        textView.setGravity(Gravity.CENTER);
+//        textView.setTextColor(Color.RED);
+//        textView.setTextSize(25);
+//        fl_content.addView(textView);
+//        //3、绑定数据
+//        textView.setText("视界内容");
 
     }
+
+
 }
